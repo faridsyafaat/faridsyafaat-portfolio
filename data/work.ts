86f564d848c47e84d53f0c27bea93525d7dd7936
faridsyafaat <faridsobang01@gmail.com>
2026-09-09 22:03:16 +0700

    Update restaurant project live URL

--- data/work.ts
+++ data/work.ts
@@ -10,12 +10,13 @@ export interface WorkItem {
 export const works: WorkItem[] = [
   {
     id: 1,
-    category: "Dashboard",
+    category: "Web Application",
     year: "2024",
-    title: "Dashboard SaaS Task Management",
-    image: "/images/work1.png",
-    url: "#",
+    title: "Restaurant Booking & Discovery App",
+    image: "/images/restaurant-home.png",
+    url: "https://restaurant-booking-app-alpha.vercel.app",
   },
+
   {
     id: 2,
     category: "Dashboard",
@@ -24,6 +25,7 @@ export const works: WorkItem[] = [
     image: "/images/work2.png",
     url: "#",
   },
+
   {
     id: 3,
     category: "Dashboard",
@@ -32,6 +34,7 @@ export const works: WorkItem[] = [
     image: "/images/work3.png",
     url: "#",
   },
+
   {
     id: 4,
     category: "Dashboard",
@@ -40,6 +43,7 @@ export const works: WorkItem[] = [
     image: "/images/work4.png",
     url: "#",
   },
+
   {
     id: 5,
     category: "Dashboard",
@@ -48,6 +52,7 @@ export const works: WorkItem[] = [
     image: "/images/work5.png",
     url: "#",
   },
+
   {
     id: 6,
     category: "Dashboard",
